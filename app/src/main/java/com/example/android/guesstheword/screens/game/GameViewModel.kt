cb99f754786ad56ee40748cc93fac938dfef411c
Eric Roberto <58601124+YokoCross@users.noreply.github.com>
2020-12-31 17:14:26 -0300

    viewmodel and viewmodelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -5,12 +5,72 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
 
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
     init {
         Log.i("GameViewModel", "GameViewModel Created")
+
+        resetList()
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel",  "GameViewModel Detroyed")
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+
+    fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 }
